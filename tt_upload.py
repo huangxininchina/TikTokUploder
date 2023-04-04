0a5d12e61e239df3ab8ea07b281e7b52f42152d8
huangxinchina <635190856@qq.com>
2023-04-04 17:30:28 +0800

    更新上传代码

--- tt_upload.py
+++ tt_upload.py
@@ -1,12 +1,30 @@
 from uploader import uploadVideo
+from deta import Deta
+import time
 
 
-session_id = 'c69bd7c225128f0b52e17e4d7b9adaaf'
-video_path = '/Users/nobody1/Downloads/final_video.mp4'
-title = 'Đồ chơi trẻ em thú vị'
-tags = '#Đồ chơi'
+deta = Deta("c0cgqjui_JYrRnnJL9539GXuwFAW59RsfMUgCL5Cd")
+to_upload = deta.Base('tt_upload')
 
-uploadVideo(session_id, video_path, title, tags)
+def upload_tasks():
+    tasks = to_upload.fetch().items
+    if len(tasks)>0:
+        for task in tasks:
+            session_id = task['session_id']
+            video_path = task['video_path']
+            title = task['title']
+            tags = task['tags']
+        # session_id = 'c69bd7c225128f0b52e17e4d7b9adaaf'
+        # video_path = '/Users/nobody1/Downloads/final_video.mp4'
+        # title = 'Đồ chơi trẻ em thú vị'
+        # tags = '#Đồ chơi'
+
+            uploadVideo(session_id, video_path, title, tags)
+    else:
+        print('没有视频要上传')
+        time.sleep(1200)
+        
 
 
-
+if __name__ == "__main__":
+    upload_tasks()
