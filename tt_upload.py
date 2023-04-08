6033089cb8cb2eaa076c5457c6a741e3b9c49c26
huangxinchina <635190856@qq.com>
2023-04-08 11:14:07 +0800

    api and tt_upload edit

--- tt_upload.py
+++ tt_upload.py
@@ -1,10 +1,51 @@
 from uploader import uploadVideo
 from deta import Deta
 import time
-
+import requests
+import json
 
 deta = Deta("c0cgqjui_JYrRnnJL9539GXuwFAW59RsfMUgCL5Cd")
-to_upload = deta.Base('tt_upload')
+to_upload = deta.Base('tk_upload')
+
+
+def get_token():
+    url = "https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal"
+    payload = json.dumps({
+        "app_id": "cli_a49ac05c8a78500c",
+        "app_secret": "8A17i1k4frBSWsHpnCI1gbYZNVSQlxhc"
+    })
+    headers = {
+    'Content-Type': 'application/json'
+    }
+    response = requests.request("POST", url, headers=headers, data=payload).json()
+    return response['tenant_access_token']
+
+
+
+
+def upload_feedback(raw_data, result):
+    url = "https://open.feishu.cn/open-apis/bitable/v1/apps/bascnkKSvx583V9CBpQ3rOEKcUf/tables/tblZB3mSz2zYnWTp/records/%s" %raw_data['record_id']
+    payload = json.dumps({
+        "fields": {
+            "发布状态": [
+                "%s" %result
+            ]
+        }
+    })
+    headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer %s' %get_token()
+    }
+
+    response = requests.request("PUT", url, headers=headers, data=payload).json()
+    if response['code'] == 0:
+        print(raw_data['record_id'], '已同步飞书')
+    else:
+        print(raw_data['record_id'], '同步飞书失败', response)
+
+
+
+
 
 def upload_tasks():
     tasks = to_upload.fetch().items
@@ -19,7 +60,16 @@ def upload_tasks():
         # title = 'Đồ chơi trẻ em thú vị'
         # tags = '#Đồ chơi'
 
-            uploadVideo(session_id, video_path, title, tags)
+            status = uploadVideo(session_id, video_path, title, tags)
+            if status:
+                print('上传成功')
+                to_upload.delete(task['key'])
+                upload_feedback(task, '已上')
+            else:
+                print('上传失败')
+                to_upload.delete(task['key'])
+                upload_feedback(task, '失败')
+
     else:
         print('没有视频要上传')
         time.sleep(1200)
